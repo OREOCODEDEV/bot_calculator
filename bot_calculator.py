4b69ba62fff785930af02b95bb3076bbd332b1f7
OREOCODEDEV <17620745509@126.COM>
2022-04-27 02:00:29 +0800

    修复群聊中包含其它文本时不会结束处理问题；优化帮助文本显示内容

--- bot_calculator.py
+++ bot_calculator.py
@@ -1,18 +1,13 @@
 from hoshino import Service
 
-sv = Service(
-    "四则运算器",
-    help_="""四则运算器\n
-    计算群聊中的四则运算消息，并发送结果\n
-    使用方法：直接发送四则运算公式；如"1+1\"""",
-)
+help_str = '计算群聊中的四则运算消息，并发送结果\n使用方法：直接发送四则运算公式；如"1+1"'
+
+sv = Service("四则运算器", help_=help_str)
 
 
 def check_message_valid(input_str):
-    valid_str_set = set(
-        ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "*", "/", ".", " "]
-    )
-    return True if input_str - valid_str_set == set([]) else False
+    valid_str_set = set(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "*", "/", ".", " "])
+    return True if set(input_str) - valid_str_set == set([]) else False
 
 
 @sv.on_message()
@@ -20,11 +15,8 @@ async def calc_message(bot, ev):
     try:
         eval_msg = str(ev.message)
         if not check_message_valid(eval_msg):
-            await bot.finish()
+            return
         result = "= " + str(eval(eval_msg))
         await bot.send(ev, result)
     except Exception as e:
-        sv.logger.error(
-            "An error occurred while trying calculate with this message:%s ,%s ,%s"
-            % (ev.message, Exception, e)
-        )
+        sv.logger.error("An error occurred while trying calculate with this message:%s ,%s ,%s" % (ev.message, Exception, e))
