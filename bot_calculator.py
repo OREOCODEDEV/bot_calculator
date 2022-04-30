96fdfc17a9b11397c1586eca49fade0c21ade116
OREOCODEDEV <17620745509@126.COM>
2022-04-30 10:39:53 +0800

    优化消息判定规则;修复发送异常bug

--- bot_calculator.py
+++ bot_calculator.py
@@ -10,6 +10,7 @@ VALID_ALL_SET = VALID_SYMBOL_SET | VALID_NUM_SET
 
 
 def check_message_valid(input_str):
+    # 因处理频率极高，出于性能考虑不使用正则匹配消息规则
     input_str_set = set(input_str)
     if input_str_set & VALID_SYMBOL_SET == set():
         # 至少包含一个四则运算符号
@@ -17,9 +18,14 @@ def check_message_valid(input_str):
     if not input_str_set - VALID_ALL_SET == set():
         # 除了四则运算不能包含其它内容
         return False
-    if {input_str[0]} & (VALID_NUM_SET | {"+", "-"}) == set():
-        # 首位必须为数字或+-
-        return False
+    # 绝大部分消息在之前都已被过滤，可以适当增加复杂判定
+    if {input_str[0]} & VALID_NUM_SET == set():
+        if {input_str[0]} & {"+", "-"} == set():
+            # 首位必须为数字或+-
+            return False
+        elif set(input_str[1:]) & VALID_SYMBOL_SET == set():
+            # 首位为+-时后续消息至少包含一个运算符号
+            return False
     if not {input_str[-1]} & VALID_SYMBOL_SET == set():
         # 不能以运算符号结尾
         return False
@@ -34,4 +40,4 @@ async def calc_message(bot, ev):
     eval_msg = str(ev.message)
     if not check_message_valid(eval_msg):
         return
-    await bot.send(ev, "= %f" + eval(eval_msg))
+    await bot.send(ev, f"= {eval(eval_msg)}")
